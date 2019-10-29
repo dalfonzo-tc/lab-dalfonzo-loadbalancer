e0fd20ebfad4c71d3449e09c37e491a701968f87
Ted Eeuwes <teeuwes@tucows.com>
2019-10-28 22:33:42 -0400

    added vip_address to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "loadbalancer_id" {
   value       = openstack_lb_loadbalancer_v2.lb.id
   description = "The ID of the created load balancer"
 }
+
+output "vip_address" {
+  value = openstack_lb_loadbalancer_v2.lb.vip_address
+  description = "The VIP Address of the load balancer"
+}
